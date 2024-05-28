8e3bb83c3189082de4b6b39578ed08926956a746
babanigit <Aniketgithub07>
2024-05-28 14:05:10 +0530

    added files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import express, { Request, Response } from 'express'
 
-console.log("hello ts")
-console.log("hello 2")
+const app = express()
+const port = process.env.PORT || 8080
 
-console.log("error")
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel')
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓')
+})
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`)
+})
